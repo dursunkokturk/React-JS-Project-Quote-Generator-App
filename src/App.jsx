c1a0ea2a1f5599a35dc4646e61528750c691fb94
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-12 13:05:23 +0300

    src klasoru icindeki app.jsx component icinde yazilar eklendi, app.css dosyasinda background-color, font-color ve gerekli css duzenlemeleri yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ export default function App() {
 
   return (
     <>
+      <div className="container">
+        <h6>Tavsiye #117</h6>
+        <h2>“Dikkatini verip farkına varmak kolaydır, zor olan ise ayağa kalkıp harekete geçmektir.”</h2>
+      </div>
     </>
   )
 }
